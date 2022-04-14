5d2edcb22ce155568ef1fc9d9dfbdbaba2ea2e42
alok <alok.chaurasia13@gmail.com>
2022-04-15 00:12:16 +0530

    paymentgetway

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,3 +1,5 @@
+const res = require('express/lib/response');
+const { default: Stripe } = require('stripe');
 const Product = require('../models/Product');
 
 const getAllProducts = async(req,res)=>{
@@ -20,7 +22,35 @@ const getProductById = async(req,res)=>{
     }
 }
 
+const getPayment = async()=>{
+    try{
+        const {product,token}=req.body;
+        const idempontencyKey = uuid()
+        return Stripe.customers.create({
+            email:token.email,
+            source:token.id
+        }).then(customer=>{
+            Stripe.charges.create({
+                amount:product.price*100,
+                currency:'usd',
+                customer:customer.id,
+                receipt_email:token.email,
+                description:`purchase of Product.name`,
+                shipping:{
+                    name:token.card.name,
+                    address:{
+                        country:token.card.address_country
+                    }
+                }
+            },{idempontencyKey})
+        }).then(result=>res.status(200).json(result))
+    }catch(error){
+console.log(error);
+    }
+}
+
 module.exports = {
     getAllProducts,
-    getProductById
+    getProductById,
+    getPayment
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const{getAllProducts,getProductById}= require('../controller/productController');
+const{getAllProducts,getProductById, getPayment}= require('../controller/productController');
 const router = express.Router();
 
 router.get('/',getAllProducts);
 
 router.get('/:id',getProductById);
+
+router.get('/payment',getPayment);
 
 module.exports = router;
